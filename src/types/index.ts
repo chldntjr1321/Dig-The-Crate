68191ec6defe107bef03c4302dd122c55dfd049c
최우석 <nick501@naver.com>
2026-06-30 16:35:04 +0900

    feat: SearchResult 타입 추가

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,16 @@ export interface Track {
   duration?: string
 }
 
+export interface SearchResult {
+  discogs_id: string
+  album_name: string
+  artist_name: string
+  cover_url: string
+  year?: string
+  genres?: string[]
+  tracklist?: Track[]
+}
+
 export interface Collection {
   id: string
   user_id: string
